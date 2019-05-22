25b090f1e9224051c3c7c77f0181187dee9355f5
sbeltran10 <s.beltran10@uniandes.edu.co>
2019-05-22 10:19:54 -0700

    favicon and minor modifications to env variables behaivour

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const http = require('http')
 
 module.exports = app => {
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.USE_SSL === '1') {
     const options = {
       cert: fs.readFileSync(process.env.CERT_FOLDER),
       key: fs.readFileSync(process.env.KEY_FOLDER)
