b6a0c809b828748fc2052e7167ef59d26a47b7f0
sbeltran10 <s.beltran10@uniandes.edu.co>
2019-06-21 20:21:48 -0700

    enable https

--- config/server.js
+++ config/server.js
@@ -1,15 +1,10 @@
 const https = require('https')
 const fs = require('fs')
-const http = require('http')
 
 module.exports = app => {
-  if (process.env.USE_SSL === '1') {
-    const options = {
-      cert: fs.readFileSync(process.env.CERT_FOLDER),
-      key: fs.readFileSync(process.env.KEY_FOLDER)
-    }
-    return https.createServer(options, app)
-  } else {
-    return http.createServer(app)
+  const options = {
+    cert: fs.readFileSync(process.env.CERT_FOLDER),
+    key: fs.readFileSync(process.env.KEY_FOLDER)
   }
+  return https.createServer(options, app)
 }
